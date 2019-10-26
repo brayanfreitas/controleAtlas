7e8d0f783b4e1d4098326a4a9a0ab254867478f4
thiag <thiag@DESKTOP-AJHH11Q>
2019-10-26 16:53:14 -0300

    Alteração, passando as coisas de programa para sistema

--- ControleEstoqueAtlas/src/estoque/controle/Sistema.java
+++ ControleEstoqueAtlas/src/estoque/controle/Sistema.java
@@ -1,6 +1,33 @@
 package estoque.controle;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import estoque.bd.ProdutoDAO;
+import estoque.classes.Marca;
+import estoque.classes.Produto;
+
 public class Sistema {
 
-}
+	private static ProdutoDAO pDAO;
 
+	public Sistema(Connection conexao) {
+		pDAO = new ProdutoDAO(conexao);
+
+		Produto p = new Produto(8, "SHUREG", 6.00, 20, "UND", new Marca(1, "Marca"));
+
+		ArrayList<Produto> produtos = pDAO.Listar();
+		for (int i = 0; i < produtos.size(); i++) {
+			System.out.println(produtos.get(i).getIdProduto());
+			System.out.println(produtos.get(i).getNomeProduto());
+			System.out.println(produtos.get(i).getPreco());
+			System.out.println("----------------------------------");
+		}
+		boolean teste = pDAO.alterar(p);
+
+		// testar LISTAR
+
+		System.out.println(teste);
+	}
+
+}
