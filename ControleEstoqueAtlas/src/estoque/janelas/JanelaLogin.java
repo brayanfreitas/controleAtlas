badb19926fd89f1ae44bae8ba80532be2ac6b7a4
thiag <thiag@DESKTOP-AJHH11Q>
2019-10-31 23:20:42 -0300

    Alteraçao da Janela Login - JDialog e campo de senha

--- ControleEstoqueAtlas/src/estoque/janelas/JanelaLogin.java
+++ ControleEstoqueAtlas/src/estoque/janelas/JanelaLogin.java
@@ -1,20 +1,30 @@
 package estoque.janelas;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JToolBar;
 import javax.swing.JPanel;
-import javax.swing.JButton;
+import javax.swing.JPasswordField;
 
-public class JanelaLogin {
 
-	private JFrame frmLogin;
-	private JTextField textField;
-	private JTextField textField_1;
+public class JanelaLogin implements ActionListener{
+
+	private JDialog frmLogin;
+	private JTextField tfUsuario;
+	private JPasswordField tfSenha;
+	private JLabel lblUsuario;
+	private JButton btnEntrar;
+	private JLabel lblLogin;
+	private JLabel lblSenha;
 
 	/**
 	 * Launch the application.
@@ -43,39 +53,67 @@ public class JanelaLogin {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmLogin = new JFrame();
+		frmLogin = new JDialog();
 		frmLogin.setTitle("Login");
 		frmLogin.setBounds(100, 100, 249, 212);
-		frmLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmLogin.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		frmLogin.getContentPane().setLayout(null);
 		
-		JLabel lblUsuario = new JLabel("Usu\u00E1rio:");
+		lblUsuario = new JLabel("Usu\u00E1rio:");
 		lblUsuario.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblUsuario.setBounds(14, 37, 57, 25);
 		frmLogin.getContentPane().add(lblUsuario);
 		
-		JLabel lblSenha = new JLabel("Senha:");
+		lblSenha = new JLabel("Senha:");
 		lblSenha.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblSenha.setBounds(14, 84, 46, 25);
 		frmLogin.getContentPane().add(lblSenha);
 		
-		textField = new JTextField();
-		textField.setBounds(70, 41, 114, 20);
-		frmLogin.getContentPane().add(textField);
-		textField.setColumns(10);
+		tfUsuario = new JTextField();
+		tfUsuario.setBounds(70, 41, 114, 20);
+		frmLogin.getContentPane().add(tfUsuario);
+		tfUsuario.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(70, 88, 114, 20);
-		frmLogin.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
+		tfSenha = new JPasswordField();
+		tfSenha.setBounds(70, 88, 114, 20);
+		frmLogin.getContentPane().add(tfSenha);
+		tfSenha.setColumns(10);
 		
-		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar = new JButton("Entrar");
+		btnEntrar.addActionListener(this);
 		btnEntrar.setBounds(81, 131, 89, 23);
 		frmLogin.getContentPane().add(btnEntrar);
 		
-		JLabel lblLogin = new JLabel("Login");
+		lblLogin = new JLabel("Login");
 		lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblLogin.setBounds(103, 11, 62, 19);
 		frmLogin.getContentPane().add(lblLogin);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object o = e.getSource();
+		if(o == btnEntrar) {
+			boolean preenchido = true;
+			if (tfUsuario.getText().equals("")) {
+				tfUsuario.setBackground(Color.PINK);
+				preenchido = false;
+			}else {
+				tfUsuario.setBackground(Color.WHITE);
+			}
+			
+			if (tfSenha.getText().equals("")) {
+				tfSenha.setBackground(Color.PINK);
+				preenchido = false;
+			}else {
+				tfSenha.setBackground(Color.WHITE);
+			}
+			if (preenchido == true) {
+				String usuario = tfUsuario.getText();
+				String senha = tfSenha.getText();
+			}
+			
+		}
+		
 	}
 }
